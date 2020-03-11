86225d3155b5ed10940a0f19386100b82d916ed0
Matt Gordon <bigmattsize21@gmail.com>
2020-03-11 18:22:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 def take_a_number (katz_deli, name)
   counter= 0
   if counter >= 0 
-    puts "Welcome," +"#{name}. You are number #{counter +1 } in line."
+    puts "Welcome," +" #{name}. You are number #{counter +1 } in line."
     counter += 1
   end
 end
