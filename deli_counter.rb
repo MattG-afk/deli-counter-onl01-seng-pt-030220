c9b367013e69a16d7d6df055593765ca47362118
Matt Gordon <bigmattsize21@gmail.com>
2020-03-11 19:58:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,9 +23,11 @@ def now_serving (katz_deli)
   if katz_deli.length == 0
   puts "There is nobody waiting to be served!"
   elsif katz_deli.length > 0
-  served= 
-  puts 
+  served= "Currently serving "
   katz_deli.each do |name|
+    served << "#{katz_deli}"
     
 end
+puts served
+
 end
