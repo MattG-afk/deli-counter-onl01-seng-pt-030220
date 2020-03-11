60d4228c9238ab8b7b5bd0b7450c9ab3faf96b5b
Matt Gordon <bigmattsize21@gmail.com>
2020-03-11 19:36:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(katz_deli)
   puts line_count
 end
 def take_a_number (katz_deli,name)
-    puts "Welcome, #{name}. You are number 1 in line."
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
     katz_deli<< name 
   end
 end
