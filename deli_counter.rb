a768267e0147509ac5e01e834acb3b885082f75d
Matt Gordon <bigmattsize21@gmail.com>
2020-03-11 18:39:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 require "pry"
 # Write your code here.
 
-def line(katz_deli)
+def line(katz_deli,name)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   elsif katz_deli.length > 0 
