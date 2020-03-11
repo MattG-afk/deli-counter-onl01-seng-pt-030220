e3bc96dd9f3c04a2fa60827ac01147b56ebf1cf9
Matt Gordon <bigmattsize21@gmail.com>
2020-03-11 16:28:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,2 +1,16 @@
 require "pry"
 # Write your code here.
+
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  end
+  
+def line(other_deli)
+  if line.length > 0 
+    puts "The line is currently: #{other_deli.index}. #{other_deli"
+     end
+  
+  end
+end
+end
