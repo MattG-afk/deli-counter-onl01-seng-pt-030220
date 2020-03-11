f310e7d8015190eea614456616cee37f5ad77883
Matt Gordon <bigmattsize21@gmail.com>
2020-03-11 18:40:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 require "pry"
 # Write your code here.
 
-def line(katz_deli,name)
+def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
   elsif katz_deli.length > 0 
@@ -11,11 +11,12 @@ def line(katz_deli,name)
   end 
   puts line_count
 end
-def take_a_number (katz_deli, name)
+def take_a_number (katz_deli,name)
   counter= 0
   if counter >= 0 
     puts "Welcome, #{name}. You are number #{counter +1 } in line."
     counter += 1
   end
+  take_a_number (katz_deli,"Ada")
 end
 end
