365ce214df6727a9927a81866154bbf49fb2b4ff
Matt Gordon <bigmattsize21@gmail.com>
2020-03-11 17:28:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,5 +13,8 @@ def line(katz_deli)
   puts line_count
 end
 def take_a_number (katz_deli, name)
-  if
+  line_count = "The line is currently:"
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  elsif katz_deli.length > 0
 end
